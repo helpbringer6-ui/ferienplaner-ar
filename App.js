df428c37b605d4b1f174ad7e91f46788aa175912
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-03 12:19:22 -0500

    feat: initial commit for App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Willkommen bei Ferienplaner AR!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
